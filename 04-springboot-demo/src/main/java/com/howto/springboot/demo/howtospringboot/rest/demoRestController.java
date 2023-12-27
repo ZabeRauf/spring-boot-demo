bc73dab1545c017f32a077b3a973a4c9502521c0
Zaben <zabe.rauf@hotmail.com>
2023-12-27 14:09:38 -0700

    add custom property injection

--- 04-springboot-demo/src/main/java/com/howto/springboot/demo/howtospringboot/rest/demoRestController.java
+++ 04-springboot-demo/src/main/java/com/howto/springboot/demo/howtospringboot/rest/demoRestController.java
@@ -1,10 +1,18 @@
 package com.howto.springboot.demo.howtospringboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class demoRestController {
+    // inject properties for team
+    @Value("${team.lead}")
+    private String teamLead;
+
+    @Value("${team.name}")
+    private String teamName;
+
     // expose "/" endpoint that returns hello beautiful
     @GetMapping("/")
     public String sayHello() {
@@ -21,5 +29,11 @@ public class demoRestController {
     @GetMapping("/study")
     public String getDailyStudy() {
         return "Study for a couple hours.";
+    }
+
+    // expose team info endpoint
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Team Lead: " + teamLead + "\n" + "Team Name: " + teamName + "\n";
     }
 }
