7193a14eaf477dd7bab6b583959024ddf251bb3a
Zaben <zabe.rauf@hotmail.com>
2023-12-30 02:31:52 -0700

    experiment with bean factory
    singleton and prototype

--- 05-springcoredemo/src/main/java/com/zoobenmcdooben/springcoredemo/common/SceneStudyCoach.java
+++ 05-springcoredemo/src/main/java/com/zoobenmcdooben/springcoredemo/common/SceneStudyCoach.java
@@ -1,6 +1,8 @@
 package com.zoobenmcdooben.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // Lazy Initialization: Instead of creating all beans up front,
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 // For this, we add the @Lazy annotation to a given class.
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SceneStudyCoach implements Coach {
 
     public SceneStudyCoach() {
--- 05-springcoredemo/src/main/java/com/zoobenmcdooben/springcoredemo/rest/demoController.java
+++ 05-springcoredemo/src/main/java/com/zoobenmcdooben/springcoredemo/rest/demoController.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class demoController {
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach myOtherCoach;
 
     // define a constructor for the dependency injection (constructor injection)
     @Autowired
-    public demoController(@Qualifier("sceneStudyCoach") Coach theCoach) {
+    public demoController(
+            @Qualifier("sceneStudyCoach") Coach theCoach,
+            @Qualifier("sceneStudyCoach") Coach theOtherCoach) {
         System.out.println("In controller: " + getClass().getSimpleName());
         myCoach = theCoach;
+        myOtherCoach = theOtherCoach;
     }
 
     // setter injection
@@ -33,4 +37,10 @@ public class demoController {
     public String getDailyMonologue() {
         return myCoach.getDailyMonologue();
     }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing Beans: myCoach == myOtherCoach, " + (myCoach == myOtherCoach);
+    }
+
 }
